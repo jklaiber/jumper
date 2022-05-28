9e4803349bf1ac1e280ff0b44cc58ff071b124ac
Julian Klaiber <julian@klaiber.me>
2022-05-28 17:08:56 +0200

    Fix bugs in which os signals are not working correctly

--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/jklaiber/jumper
 go 1.16
 
 require (
+	github.com/docker/docker v20.10.16+incompatible
+	github.com/moby/term v0.0.0-20210619224110-3f7ff695adc6 // indirect
 	github.com/sosedoff/ansible-vault-go v0.1.1
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.1
--- pkg/connection/connection.go
+++ pkg/connection/connection.go
@@ -1,113 +1,19 @@
 package connection
 
 import (
+	"context"
 	"fmt"
-	"io"
 	"io/ioutil"
+	"log"
 	"net"
 	"os"
-	"strings"
+	"os/signal"
+	"syscall"
 
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/agent"
+	"golang.org/x/crypto/ssh/terminal"
 )
-
-type SSHCommand struct {
-	Path   string
-	Env    []string
-	Stdin  io.Reader
-	Stdout io.Writer
-	Stderr io.Writer
-}
-
-type SSHClient struct {
-	Config *ssh.ClientConfig
-	Host   string
-	Port   int
-}
-
-func (client *SSHClient) RunCommand(cmd *SSHCommand) error {
-	var (
-		session *ssh.Session
-		err     error
-	)
-
-	if session, err = client.newSession(); err != nil {
-		return err
-	}
-	defer session.Close()
-
-	if err = client.prepareCommand(session, cmd); err != nil {
-		return err
-	}
-
-	err = session.Run(cmd.Path)
-	return err
-}
-
-func (client *SSHClient) prepareCommand(session *ssh.Session, cmd *SSHCommand) error {
-	for _, env := range cmd.Env {
-		variable := strings.Split(env, "=")
-		if len(variable) != 2 {
-			continue
-		}
-
-		if err := session.Setenv(variable[0], variable[1]); err != nil {
-			return err
-		}
-	}
-
-	if cmd.Stdin != nil {
-		stdin, err := session.StdinPipe()
-		if err != nil {
-			return fmt.Errorf("unable to setup stdin for session: %v", err)
-		}
-		go io.Copy(stdin, cmd.Stdin)
-	}
-
-	if cmd.Stdout != nil {
-		stdout, err := session.StdoutPipe()
-		if err != nil {
-			return fmt.Errorf("unable to setup stdout for session: %v", err)
-		}
-		go io.Copy(cmd.Stdout, stdout)
-	}
-
-	if cmd.Stderr != nil {
-		stderr, err := session.StderrPipe()
-		if err != nil {
-			return fmt.Errorf("unable to setup stderr for session: %v", err)
-		}
-		go io.Copy(cmd.Stderr, stderr)
-	}
-
-	return nil
-}
-
-func (client *SSHClient) newSession() (*ssh.Session, error) {
-	connection, err := ssh.Dial("tcp", fmt.Sprintf("%s:%d", client.Host, client.Port), client.Config)
-	if err != nil {
-		return nil, fmt.Errorf("failed to dial: %s", err)
-	}
-
-	session, err := connection.NewSession()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create session: %s", err)
-	}
-
-	modes := ssh.TerminalModes{
-		ssh.ECHO:          0,     // disable echoing
-		ssh.TTY_OP_ISPEED: 14400, // input speed = 14.4kbaud
-		ssh.TTY_OP_OSPEED: 14400, // output speed = 14.4kbaud
-	}
-
-	if err := session.RequestPty("xterm", 80, 40, modes); err != nil {
-		session.Close()
-		return nil, fmt.Errorf("request for pseudo terminal failed: %s", err)
-	}
-
-	return session, nil
-}
 
 func PublicKeyFile(file string) ssh.AuthMethod {
 	buffer, err := ioutil.ReadFile(file)
@@ -156,26 +62,87 @@ func NewConnection(username string, host string, password string, sshkey string,
 		}
 	}
 
-	client := &SSHClient{
-		Config: sshConfig,
-		Host:   host,
-		Port:   22,
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	fmt.Printf("Connecting to %s with %s\n\n", host, username)
+	go func() {
+		if err := runShell(ctx, host, sshConfig); err != nil {
+			log.Print(err)
+		}
+		cancel()
+	}()
+
+	select {
+	case <-sig:
+		cancel()
+	case <-ctx.Done():
 	}
 
-	cmd := &SSHCommand{
-		Path:   "bash",
-		Env:    []string{"bash"},
-		Stdin:  os.Stdin,
-		Stdout: os.Stdout,
-		Stderr: os.Stderr,
-	}
+	return nil
+}
 
-	fmt.Printf("Connecting to %s with %s\n\n", client.Host, sshConfig.User)
-
-	err := client.RunCommand(cmd)
+func runShell(ctx context.Context, host string, sshConfig *ssh.ClientConfig) error {
+	conn, err := ssh.Dial("tcp", host+":22", sshConfig)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "command run error: %s\n", err)
-		return err
+		log.Fatalf("error")
+	}
+	defer conn.Close()
+
+	session, err := conn.NewSession()
+	if err != nil {
+		return fmt.Errorf("cannot open new session: %v", err)
+	}
+	defer session.Close()
+
+	go func() {
+		<-ctx.Done()
+		conn.Close()
+	}()
+
+	fd := int(os.Stdin.Fd())
+	state, err := terminal.MakeRaw(fd)
+	if err != nil {
+		return fmt.Errorf("terminal make raw: %s", err)
+	}
+	defer terminal.Restore(fd, state)
+
+	w, h, err := terminal.GetSize(fd)
+	if err != nil {
+		return fmt.Errorf("terminal get size: %s", err)
+	}
+
+	modes := ssh.TerminalModes{
+		ssh.ECHO:          1,
+		ssh.TTY_OP_ISPEED: 14400,
+		ssh.TTY_OP_OSPEED: 14400,
+	}
+
+	term := os.Getenv("TERM")
+	if term == "" {
+		term = "xterm-256color"
+	}
+	if err := session.RequestPty(term, h, w, modes); err != nil {
+		return fmt.Errorf("session xterm: %s", err)
+	}
+
+	session.Stdout = os.Stdout
+	session.Stderr = os.Stderr
+	session.Stdin = os.Stdin
+
+	if err := session.Shell(); err != nil {
+		return fmt.Errorf("session shell: %s", err)
+	}
+
+	if err := session.Wait(); err != nil {
+		if e, ok := err.(*ssh.ExitError); ok {
+			switch e.ExitStatus() {
+			case 130:
+				return nil
+			}
+		}
+		return fmt.Errorf("ssh: %s", err)
 	}
 	return nil
 }

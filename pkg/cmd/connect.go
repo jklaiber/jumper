0faf7dae53fe43529418688f1e1bc12f3d515b0b
Julian Klaiber <julian@klaiber.me>
2023-12-01 08:29:46 +0100

    Fix lint error

--- pkg/cmd/connect.go
+++ pkg/cmd/connect.go
@@ -33,7 +33,7 @@ var Group string
 var connectCmd = &cobra.Command{
 	Use:   "connect",
 	Short: "Connect to a saved connection",
-	Args:  cobra.ExactValidArgs(1),
+	Args:  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		accessConfig, err := inv.GetAccessConfig(Group, args[0])
 		if err != nil {

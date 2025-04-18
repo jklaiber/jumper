054817e5bc6fe9d67502605d417ec0d3b1fc9d0c
Julian Klaiber <julian@klaiber.me>
2025-04-18 13:24:01 +0200

    Update version

--- internal/version/version.go
+++ internal/version/version.go
@@ -4,7 +4,7 @@ import "fmt"
 
 const MAJOR uint = 0
 const MINOR uint = 1
-const PATCH uint = 0
+const PATCH uint = 3
 
 func GetVersion() string {
 	return fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
